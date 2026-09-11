257f8e957fcf9dfb91705a16a2b86169149e048d
rissshu <rishuraj24042000@gmail.com>
2026-09-11 22:47:12 +0530

    window state plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -307,7 +307,10 @@ pub fn run() {
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_process::init())
-        .setup(|_app| {
+        .setup(|app| {
+            #[cfg(desktop)]
+            app.handle()
+                .plugin(tauri_plugin_window_state::Builder::default().build())?;
 
             Ok(())
         })
