c53aeafd518633d5a4cf441d7416d36f009dbc4f
rissshu <rishuraj24042000@gmail.com>
2026-09-12 00:49:54 +0530

    fixed window state management

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -307,11 +307,8 @@ pub fn run() {
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_process::init())
-        .setup(|app| {
-            #[cfg(desktop)]
-            app.handle()
-                .plugin(tauri_plugin_window_state::Builder::default().build())?;
-
+        .plugin(tauri_plugin_window_state::Builder::default().build())
+        .setup(|_app| {
             Ok(())
         })
 
